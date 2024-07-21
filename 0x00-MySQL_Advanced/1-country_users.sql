58f4cb3860d5051ffd1ecd837f3bdbab66f11374
tolu13 <tolulopeolanrewaju8@gmail.com>
2024-07-21 07:00:58 +0000

    advanced sql

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE IF NOT EXISTS users (
+    id int NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
